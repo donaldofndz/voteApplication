8f8f4b653c24b33c0fc28a96c2ff9435bb6d0cb7
José Donaldo Fernández Montenegro <donaldo@MacBook-Pro-de-Jose.local>
2020-03-29 22:35:23 -0600

    Updates

---
+++ precache-manifest.fabf1485f6000a6ce42557992516bba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a143695a135560957eb55cabc396696",
+    "url": "/voteapplication/index.html"
+  },
+  {
+    "revision": "844fa22ec6b0eacb149e",
+    "url": "/voteapplication/static/css/main.08d0dd61.chunk.css"
+  },
+  {
+    "revision": "a2f617cc11f5b87a071e",
+    "url": "/voteapplication/static/js/2.4df14e32.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/voteapplication/static/js/2.4df14e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844fa22ec6b0eacb149e",
+    "url": "/voteapplication/static/js/main.a1cf0b3d.chunk.js"
+  },
+  {
+    "revision": "de9ab8c4da35db655ca3",
+    "url": "/voteapplication/static/js/runtime-main.deb537c4.js"
+  }
+]);
